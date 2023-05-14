a161865fff5559dc59ab9d91c88b724af787464e
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2023-05-14 20:48:00 +0700

    feat : login

--- ryxcafe/routes/web.php
+++ ryxcafe/routes/web.php
@@ -15,17 +15,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get("/home", [HomeController::class,"index"]);
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
+
+Route::get("/", [HomeController::class,"index"]);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () 
+{
         return view('dashboard');
     })->name('dashboard');
-});

ca5fb1b5b27eee229e5a5ef7eff7bbe6fb3a6beb
Rahmatul idami <rahmatul_i@mhs.unsyiah.ac.id>
2023-05-17 20:43:17 +0700

    fix: admin food

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get("/", [HomeController::class,"index"]);
 
 Route::get("/users", [AdminController::class,"user"]);
 
+Route::get("/foodmenu", [AdminController::class,"foodmenu"]);
+
+Route::post("/uploadfood", [AdminController::class,"upload"]);
+
 Route::get("/deleteuser/{id}", [AdminController::class,"deleteuser"]);
 
 Route::get("/redirects", [HomeController::class,"redirects"]);
